95e2bd55dfaa0add31278217330642ce40cef477
Nima <nima97e@gmail.com>
2024-04-19 12:11:54 +0330

    Make search placeholder dynamic

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -38,8 +38,7 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
     };
 
     const url = new URL(
-      `${
-        isProd ? "https://streams-on.vercel.app" : "http://localhost:4321/"
+      `${isProd ? "https://streams-on.vercel.app" : "http://localhost:4321/"
       }/api/${inputValue.value}?media=${searchMedia.value}`
     );
 
@@ -53,9 +52,8 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
   return (
     <div class="relative z-20">
       <div
-        class={`flex gap-2 p-2 justify-end items-center ${
-          isHomepage ? "justify-center" : ""
-        }`}
+        class={`flex gap-2 p-2 justify-end items-center ${isHomepage ? "justify-center" : ""
+          }`}
       >
         {isHomepage && <p>What are you looking for?</p>}
         <div>
@@ -106,15 +104,14 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
           ref={inputRef}
           class="border-[3px] border-black outline-none ring-0 border-opacity-0 rounded-sm sm:rounded-xl px-2 py-1 lg:py-2 lg:min-w-96 w-full bg-zinc-50 text-zinc-900 placeholder-zinc-600 focus:outline-none focus:border-orange-600 hover:border-zinc-500 focus:border-[3px] transition-all"
           type="text"
-          placeholder="Search"
+          placeholder={`Search ${searchMedia.value == 'tv' ? "Series" : "Movies"}`}
           value={inputValue.value}
           onInput$={(_, element) => (inputValue.value = element.value)}
         />
 
         <button
-          class={`${
-            inputValue.value.length < 1 && "hidden"
-          } absolute inline-block text-2xl font-bold right-2 top-1/2 -translate-y-1/2 text-zinc-900 hover:text-zinc-700`}
+          class={`${inputValue.value.length < 1 && "hidden"
+            } absolute inline-block text-2xl font-bold right-2 top-1/2 -translate-y-1/2 text-zinc-900 hover:text-zinc-700`}
           onClick$={() => {
             inputValue.value = "";
             inputRef.value?.focus();
@@ -146,15 +143,12 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
         )}
         onResolved={(items) => (
           <ul
-            class={`absolute overflow-y-auto overflow-x-hidden left-0 right-0 divide-y-2 divide-zinc-900 divide-opacity-40 rounded-md bg-zinc-950 text-orange-600 ${
-              items.length != 0 && "p-1"
-            }  ${items.length === 1 && "h-24"} ${
-              items.length === 2 && "h-44"
-            } ${items.length === 3 && "h-72"} ${
-              items.length > 3 && isHomepage
+            class={`absolute overflow-y-auto overflow-x-hidden left-0 right-0 divide-y-2 divide-zinc-900 divide-opacity-40 rounded-md bg-zinc-950 text-orange-600 ${items.length != 0 && "p-1"
+              }  ${items.length === 1 && "h-24"} ${items.length === 2 && "h-44"
+              } ${items.length === 3 && "h-72"} ${items.length > 3 && isHomepage
                 ? "h-[50vh]"
                 : items.length > 3 && "h-[70vh]"
-            } `}
+              } `}
           >
             {items.map((item, i) => (
               <li
@@ -164,9 +158,8 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
                 <a
                   class="w-full h-full grid grid-cols-[1fr_5fr] gap-2"
                   onClick$={() => (inputValue.value = "")}
-                  href={`/${
-                    searchMedia.value === "movie" ? "movie" : "series"
-                  }/${item.id}`}
+                  href={`/${searchMedia.value === "movie" ? "movie" : "series"
+                    }/${item.id}`}
                 >
                   <div class="w-8 lg:w-12">
                     <img
@@ -176,9 +169,8 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
                           ? `https://media.themoviedb.org/t/p/original${item.poster_path}`
                           : "../../imagePlaceholder.png"
                       }
-                      alt={`Movieposter of ${
-                        "name" in item ? item.name : item.title
-                      }`}
+                      alt={`Movieposter of ${"name" in item ? item.name : item.title
+                        }`}
                     />
                   </div>
 
@@ -186,11 +178,11 @@ export const Search = component$(({ isHomepage }: { isHomepage: boolean }) => {
                     <p class="">{"name" in item ? item.name : item.title}</p>
                     {"name" in item
                       ? item.original_name !== item.name && (
-                          <p class="">{item.original_name}</p>
-                        )
+                        <p class="">{item.original_name}</p>
+                      )
                       : item.original_title !== item.title && (
-                          <p class="">{item.original_title}</p>
-                        )}
+                        <p class="">{item.original_title}</p>
+                      )}
                   </div>
                 </a>
               </li>
